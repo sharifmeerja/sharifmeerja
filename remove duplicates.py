9823525c863197374b78861529f588a95466ba13
sharifmeerja <99115865+sharifmeerja@users.noreply.github.com>
2022-02-17 15:51:34 +0530

    Add files via upload

---
+++ remove duplicates.py
@@ -0,0 +1,8 @@
+def removeduplicates(x):
+    y=[]
+    for i in x:
+        if i not in y:
+            y.append(i)
+        else:
+            pass
+    print(y)
---
+++ reverse in python.py
@@ -0,0 +1,7 @@
+def reverse(self,x):
+    y=[]
+    for i in range(len(x)):
+        y.append(x[len(x)-i-1])
+    print(y)
+
+    
